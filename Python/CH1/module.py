53223c38ea85c87344700025f48e2ee92e5da745
Vikash Gupta <vikash9c35@gmail.com>
2025-06-12 20:18:43 +0530

    Add files via upload

---
+++ Python/CH1/module.py
@@ -0,0 +1,9 @@
+import pyjokes
+
+# print("Printing Joke...")
+joke = pyjokes.get_joke()
+print(joke)
+
+"""
+Here CH 1 ends !!?
+"""
---
+++ Python/CH1/practice-set/os.py
@@ -0,0 +1,11 @@
+import os
+
+# root directory location
+directory ='/'
+
+# Get the contents of the root directory
+contents = os.listdir(directory)
+
+# Print the contents of the root directory
+# for item in contents:
+print(contents)
