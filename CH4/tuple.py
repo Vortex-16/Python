0cd10ff96405c3286e2d49273b09d80a7178f67d
Vikash Gupta <vikasharmy811@gmail.com>
2025-07-26 16:05:48 +0530

    Lecture 4

--- CH4/tuple.py
+++ CH4/tuple.py
@@ -1,3 +1,7 @@
-a=(6, 9 ,"hello", 3.14, True, None)
-a[0]=10  # This will raise an error because tuples are immutable
+a = (6, 9, "hello", 3.14, True, None)
+# a[0] = 10  
+# This will raise an error because tuples are immutable
 print(type(a))
+i = a.index("hello")
+print(i)
+print("hello" in a)
