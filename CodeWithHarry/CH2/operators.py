9b3ee2f8b2290d26148d1d2e5c0e6b8296b3a8e7
Vikash Gupta <vikasharmy811@gmail.com>
2025-08-28 12:18:43 +0530

    Update operators.py
    Going on

--- CodeWithHarry/CH2/operators.py
+++ CodeWithHarry/CH2/operators.py
@@ -14,3 +14,28 @@ d = True or False
 print(d)
 
 print(not(True))
+
+
+try:
+    print(x)
+except NameError:
+    print("Variable x is not defined.")
+
+# Exception handling
+'''
+try:
+    print(5/0)
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.")
+
+    # Handle other exceptions
+    except Exception as e:
+        print("Error:", e)
+        print("An unexpected error occurred.")
+        print("Please try again later.")
+
+    finally:
+        print("Cleanup actions can be performed here.")
+'''
+
+# Exception handling
