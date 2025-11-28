a1688c140cc568a6c329673d1f29cbbfcb4736cd
Vikash Gupta <vikasharmy811@gmail.com>
2025-11-28 18:19:23 +0530

    hit&trial

--- At Home/GUI/Basic/ChatGpt4.py
+++ At Home/GUI/Basic/ChatGpt4.py
@@ -6,4 +6,12 @@ root.geometry("400x300")
 entry = tk.Entry(root, font=("Arial", 14))
 entry.pack()
 
+def say_hello():
+    value= entry.get()
+    print("You entered:", value)
+
+btn = tk.Button(root, text="Click Me", command=say_hello)
+btn.pack()
+
 root.mainloop()
+
